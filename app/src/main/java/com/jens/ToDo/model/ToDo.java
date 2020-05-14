7130c0bc6e451dd6b9df696a826f861305031836
Jens <jens.roecker@gmail.com>
2020-05-14 22:41:07 +0200

    - Created Empty Actitvity, Added ToDo class

---
+++ app/src/main/java/com/jens/ToDo/model/ToDo.java
@@ -0,0 +1,80 @@
+package com.jens.ToDo.model;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+import androidx.room.TypeConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class ToDo {
+
+    @PrimaryKey(autoGenerate = true)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean done =false;
+    private boolean favourite =false;
+    private Long expiry;
+    private List<String> contactList = new ArrayList<>();
+    public ToDo(){
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    public boolean isFavourite() {
+        return favourite;
+    }
+
+    public void setFavourite(boolean favourite) {
+        this.favourite = favourite;
+    }
+
+    public Long getExpiry() {
+        return expiry;
+    }
+
+    public void setExpiry(Long expiry) {
+        this.expiry = expiry;
+    }
+
+    public List<String> getContactList() {
+        return contactList;
+    }
+
+    public void setContactList(List<String> contactList) {
+        this.contactList = contactList;
+    }
+}
