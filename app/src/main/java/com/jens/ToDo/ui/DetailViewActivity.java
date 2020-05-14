b6083d526dd34c0d90e3b094f82aa106a65891aa
Jens <jens.roecker@gmail.com>
2020-05-14 22:50:45 +0200

    added DetailView, menu, icons

---
+++ app/src/main/java/com/jens/ToDo/ui/DetailViewActivity.java
@@ -0,0 +1,16 @@
+package com.jens.ToDo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.jens.ToDo.R;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+    }
+}
