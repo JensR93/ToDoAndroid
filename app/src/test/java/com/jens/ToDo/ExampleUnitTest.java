365df02b72dea5eeaa986da6182b6ea5966af9f5
Jens <jens.roecker@gmail.com>
2020-05-20 20:11:55 +0200

    Added icons

--- app/src/test/java/com/jens/ToDo/ExampleUnitTest.java
+++ app/src/test/java/com/jens/ToDo/ExampleUnitTest.java
@@ -1,6 +1,44 @@
 package com.jens.ToDo;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.view.Display;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.jens.ToDo.model.impl.RoomToDoCRUDOperationsImpl;
+import com.jens.ToDo.model.interfaces.IToDoCRUDOperations;
+
 import org.junit.Test;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +48,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+    private IToDoCRUDOperations crudOperations;
+
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void localCrudInit(){
+
+        // this.crudOperations=new RoomToDoCRUDOperationsImpl(c);
+       // assertNotNull(crudOperations);
     }
 }
